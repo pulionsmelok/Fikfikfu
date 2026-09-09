9ec9fa86654454b8d1e8789b3bad414dad24af5d
pulionsmelok <itssiddik487@gmail.com>
2026-09-09 15:32:53 +0600

    Add files via upload

--- database/connectDB/connectMongoDB.js
+++ database/connectDB/connectMongoDB.js
@@ -1,4 +1,7 @@
-module.exports = async function (uriConnect) {\n\turiConnect = process.env.MONGODB_URI || uriConnect;\n\tif (!uriConnect || uriConnect === "your_mongodb-uri" || uriConnect === "YOUR_MONGODB_URI")\n\t\tthrow new Error("MongoDB URI is not configured. Set database.uriMongodb or MONGODB_URI.");
+module.exports = async function (uriConnect) {
+	uriConnect = process.env.MONGODB_URI || uriConnect;
+	if (!uriConnect || uriConnect === "your_mongodb-uri" || uriConnect === "YOUR_MONGODB_URI")
+		throw new Error("MongoDB URI is not configured. Set database.uriMongodb or MONGODB_URI.");
 	const mongoose = require("mongoose");
 
 	const threadModel = require("../models/mongodb/thread.js");
